7b28ef65b15d5a4058b9e0e6cb5238ace6092126
YanDu <53807965+vmfpdla@users.noreply.github.com>
2020-01-15 12:21:09 +0900

    Update NO_11399.java

--- NO_11399.java
+++ NO_11399.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class NO_11399 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // sc 라는 인스턴스 생성
 		int n;
 		int sum=0;
 		n=sc.nextInt();
-		int[] num = new int[n];
+		int[] num = new int[n]; // 배열의 생성과 선언을 동시에 . C언어와 다르게 변수 N을 이용하여 바로 배열의 크기 지정이 가능
 		for(int i=0;i<n;i++) num[i] =sc.nextInt();
-		Arrays.sort(num);
+		Arrays.sort(num); //Arrays.sort(배열이름) 하면 자동으로 오름차순 정렬
 		for(int i=0;i<n;i++){
 			for(int j=0;j<=i;j++) sum=sum+num[j]; 
 		}
